c1b83f1259a5bed991f5c9afe619ce5c4f024b2a
Tony Ivanov <telamohn@pm.me>
2020-08-20 10:36:44 +0200

    Added prompt to the publishing script

--- hyper-publish.js
+++ hyper-publish.js
@@ -1,3 +1,4 @@
+const readline = require('readline')
 const minimist = require('minimist')
 const replicate = require('@hyperswarm/replicator')
 const hyperdrive = require('hyperdrive')
@@ -7,46 +8,27 @@ const { createHash } = require('crypto')
 const { machineId } = require('node-machine-id')
 const { defer } = require('deferinfer')
 
-
 const args = minimist(process.argv.slice(2), {
   string: 'secret',
-  boolean: [ 'help' ],
-  alias: { h: 'help', sk: 'secret' },
+  boolean: ['help', 'dry-run', 'break-lock'],
+  alias: { h: 'help', sk: 'secret', n: 'dry-run', f: 'break-lock' },
   default: { lang: 'en' }
 })
 const root = args._.shift()
 
 async function main () {
-  const mid = await machineId()
-  const hardwareIdentifier = createHash('sha256')
-    .update(mid)
-    .digest()
-  /* Shared drives, easy peasy:
-   * echo hwID > /drive/.hwlock
-   * if .lock != hwID:
-   *   lockreq = sign(secret, 'Plz gief lock to ${hwId} ${@seq}`)
-   * onLockRequest ()
-   *  if this.hwId == .hwlock && seq == req.seq:
-   *    echo req.hwid > .hwlock
-   *
-   * Functionality as a module api:
-   *
-   * drive = hyperdrive(path, { extensions: [hyperlock] })
-   * const mine = await hyperlock(drive, TIMEOUT = 30*1000)
-   * if (mine) drive.write(....)
-   */
   if (!root) throw new Error('Positional argument "path" missing')
   const drive = hyperdrive('.hyper_dist/')
   await defer(d => drive.ready(d))
   replicate(drive)
-  // There's no param to inject it during initialization.
-  const secret = drive.metadata.secretKey.hexSlice()
   await syncFolder(drive, root)
   console.log(`hyper://${drive.key.hexSlice()}`, '@', drive.version)
   console.log('Seeding drive until stopped (Ctrl+C)')
 }
+
 async function syncFolder (drive, path) {
   const files = readdirSync(path)
+  const tasks = []
   for (const file of files) {
     const src = join(path, file)
     const dst = relative(root, src)
@@ -62,19 +44,72 @@ async function syncFolder (drive, path) {
     const hash = createHash('md5')
       .update(content)
       .digest()
-    const dstat = await defer(d => drive.lstat(dst, { wait: true }, d))
-      .catch(err => { if(err.code !== 'ENOENT') throw err })
-    let overwrite = true
-    if (dstat && dstat.metadata && dstat.metadata.checksum) {
-      if(hash.equals(dstat.metadata.checksum)) overwrite = false
-    }
 
-    if (overwrite) {
-      const metadata = { checksum: hash }
-      await defer(done => drive.writeFile(dst, content, { metadata }, done))
-      console.log('Imported', hash.hexSlice(), dst)
+    const dstat = await defer(d => drive.lstat(dst, { wait: true }, d))
+      .catch(err => { if (err.code !== 'ENOENT') throw err })
+    let write = true
+    if (dstat && dstat.metadata && dstat.metadata.checksum) {
+      if (hash.equals(dstat.metadata.checksum)) write = false
     }
+    if (write) tasks.push({ content, dst, hash, size: stat.size })
   }
+
+  if (!tasks.length) {
+    console.log('Nothing to do, drive already in sync')
+    return
+  }
+  console.log('The Following files are about to be imported:')
+  for (const { dst, hash, size } of tasks) {
+    console.log(hash.hexSlice(), size, dst)
+  }
+
+  if (args.n) return // Dry-running
+
+  await defer(done => {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    })
+    rl.question('Proceed with import? (y/N)', line => {
+      if (line.match(/^y(es)?$/)) done()
+      else {
+        // done(new Error('Aborted by user'))
+        console.log('Aborted by user')
+        process.exit(0)
+      }
+    })
+  })
+
+  for (const { content, dst, hash } of tasks) {
+    const metadata = { checksum: hash }
+    await defer(done => drive.writeFile(dst, content, { metadata }, done))
+  }
+}
+
+const LOCK_KEY = '.device.lock'
+async function unlock (drive) {
+  // There's no param to inject it during initialization.
+  const secret = drive.metadata.secretKey.hexSlice()
+
+  const deviceId = await machineId()
+    .then(mid => createHash('sha256')
+      .update(mid)
+      .digest())
+
+  /* Shared drives, easy peasy:
+   * echo hwID > /drive/.hwlock
+   * if .lock != hwID:
+   *   lockreq = sign(secret, 'Plz gief lock to ${hwId} ${current.seq}`)
+   * onLockRequest ()
+   *  if this.hwId == .hwlock && seq == req.seq:
+   *    echo req.hwid > .hwlock
+   *
+   * Functionality as a module api:
+   *
+   * drive = hyperdrive(path, { extensions: [hyperlock] })
+   * const isMine = await hyperlock(drive, TIMEOUT = 30*1000)
+   * if (mine) drive.write(....)
+   */
 }
 
 main()
